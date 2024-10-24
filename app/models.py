3b6d5b18099195e3c4accb0c646a3eb0ee06ca3c
GabrielBarrios17 <gabibarrios.33@gmail.com>
2024-10-24 00:56:22 -0300

    creacion de modelo

--- app/models.py
+++ app/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+class Rol(models.Model):
+    nombre = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nombre
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    contraseña = models.CharField(max_length=255)
+    rol = models.ForeignKey(Rol, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Noticia(models.Model):
+    titulo = models.CharField(max_length=255)
+    contenido = models.TextField()
+    autor = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    fecha_publicacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
+
+class Reseña(models.Model):
+    titulo = models.CharField(max_length=255)
+    contenido = models.TextField()
+    autor = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    pelicula = models.CharField(max_length=255)
+    fecha_publicacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
+
+class Comentario(models.Model):
+    contenido = models.TextField()
+    autor = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    noticia = models.ForeignKey(Noticia, on_delete=models.CASCADE, null=True, blank=True)
+    reseña = models.ForeignKey(Reseña, on_delete=models.CASCADE, null=True, blank=True)
+    fecha_publicacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comentario por {self.autor} en {self.fecha_publicacion}"
